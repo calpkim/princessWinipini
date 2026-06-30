72cba2edbcde4ebbabf2072103070e33833f3050
calpkim-debian <pablillo.kim@gmail.com>
2026-06-30 11:02:32 -0700

    edited to use stepper code, still doesn't work

---
+++ ReducedTimonCode/ReducedTimonCode.ino
@@ -0,0 +1,33 @@
+#include <Stepper.h>
+const int receiverPin = 2;
+volatile unsigned long pulseStart = 0;
+volatile unsigned long pulseWidth = 0;
+int currentStep = 0;
+int targetStep = 0;
+const int stepsPerRevolution = 200;
+Stepper mystepper(stepsPerRevolution, 5, 6, 7, 8);
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(receiverPin, INPUT);
+    mystepper.setSpeed(30);
+    attachInterrupt(digitalPinToInterrupt(receiverPin), readPulse, CHANGE);
+}
+
+void loop() {
+    targetStep = map(pulseWidth, 996, 1996, 0, 50);
+    mystepper.step(5000);
+    currentStep = targetStep;
+    Serial.print(targetStep);
+    Serial.print("             ");
+    Serial.println(currentStep);
+
+}
+
+void readPulse() {
+    if (digitalRead(receiverPin)==HIGH) {
+        pulseStart = micros();
+    } else{
+        pulseWidth = micros() - pulseStart;
+    }
+}
--- newTimonCode/newTimonCode.ino
+++ newTimonCode/newTimonCode.ino
@@ -1,44 +1,56 @@
-// Read PWM signals from FS-iA6B using Arduino Uno
+#include <Stepper.h>
 const int receiverPin = 2;
 volatile unsigned long pulseStart = 0;
 volatile unsigned long pulseWidth = 0;
+/*
 int Sin1 = 5; //0;
 int Sin2 = 6; //13;
 int Sin3 = 7; //15;
 int Sin4 = 8; //12;
 int dutyCycle = 0;
-const int stepsPerRevolution = 200; // 1.8° per step
-int currentStep = 0;
-int targetStep = 0;
+
 int steering = 0;
 
+*/
+int currentStep = 0;
+int targetStep = 0;
+const int stepsPerRevolution = 200; // 1.8° per step
+Stepper mystepper(stepsPerRevolution, 5, 6, 7, 8);
+
+
 void setup() {
+    Serial.begin(9600);
     pinMode(receiverPin, INPUT);
-    //Serial.begin(115200);
+    /*Serial.begin(115200);
     pinMode(Sin1, OUTPUT);
     pinMode(Sin2, OUTPUT);
     pinMode(Sin3, OUTPUT);
     pinMode(Sin4, OUTPUT);
-//    analogWriteRange(100);
-    Serial.begin(9600);
+    analogWriteRange(100); */
+    mystepper.setSpeed(30);
     attachInterrupt(digitalPinToInterrupt(receiverPin), readPulse, CHANGE);
-    Serial.println("Hast Set Up ish");
 }
 
 void loop() {
  //   Serial.print("PulseWidth               ");
- //   Serial.println(steering);
+ //  Serial.println(steering);
  //   Serial.println("us");
     
- 
+    targetStep = map(pulseWidth, 996, 1996, 0, 50);
+    mystepper.step(5000);
+    currentStep = targetStep;
+    Serial.print(targetStep);
+    Serial.print("             ");
+    Serial.println(currentStep);
+    /*
     steering = (pulseWidth + 8)/10 * 10;
     int angleSteering = map(steering, 996, 1996, 0, 360);
     targetStep = map(angleSteering, 0, 360, 0, stepsPerRevolution);
-    Serial.println(targetStep);
-    if (abs(targetStep - currentStep) < 5) {
+    
 
+    if (abs(targetStep - currentStep) < 5) {
     } else {
-        //while (currentStep != targetStep) {
+        while (currentStep != targetStep) {
           if (targetStep > currentStep) {
           stepForward();
           currentStep++;
@@ -46,10 +58,11 @@ void loop() {
           stepBackward();
           currentStep--;
           }
-        //delay(5); // Controls speed
-        //}
+        delay(5);
+        }
     }
-//    delay(100); 
+    */
+
 }
 
 void readPulse() {
@@ -59,12 +72,12 @@ void readPulse() {
         pulseWidth = micros() - pulseStart;
     }
 }
-
+/*
 void stepForward() {
 /*  static int step = 0;
   step = (step + 1) % 4;
   setStep(step);
-  */
+  
   setStep(currentStep % 4);
 }
 
@@ -92,3 +105,4 @@ void setStep(int step) {
       break;
   }
 }
+*/
