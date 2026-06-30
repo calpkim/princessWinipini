a738a5d0fd1baa75a651a2d0b7e4c802b555f17e
calpkim-debian <pablillo.kim@gmail.com>
2026-06-30 12:07:02 -0700

    just added a anti jitter trhing

--- ReducedTimonCode/ReducedTimonCode.ino
+++ ReducedTimonCode/ReducedTimonCode.ino
@@ -1,9 +1,9 @@
 #include <Stepper.h>
-const int receiverPin = 2;
+const int receiverPin = 11;
 volatile unsigned long pulseStart = 0;
 volatile unsigned long pulseWidth = 0;
 int currentStep = 0;
-int targetStep = 0;
+int targetStep = 25;
 const int stepsPerRevolution = 200;
 Stepper mystepper(stepsPerRevolution, 5, 6, 7, 8);
 
@@ -16,8 +16,11 @@ void setup() {
 
 void loop() {
     targetStep = map(pulseWidth, 996, 1996, 0, 50);
+    if (-5 < targetStep - currentStep < 5) {
+    } else {
     mystepper.step(targetStep - currentStep);
     currentStep = targetStep;
+    }
     Serial.print(targetStep);
     Serial.print("             ");
     Serial.println(currentStep);
